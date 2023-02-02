d33c1d8accb37e07081d9a0c427dbe26cfcda884
PARK GI-PYO <gipyopark@gmail.com>
2023-02-02 18:36:48 +0900

    [init] #1 : 스프링 부트 프로젝트 환경 구성
    스프링 부트 프로젝트를 생성하고, 기본적인 의존성을 추가해 환경 구성을 완료

---
+++ src/main/java/com/example/projectpoi/ProjectPoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPoiApplication.class, args);
+    }
+
+}
