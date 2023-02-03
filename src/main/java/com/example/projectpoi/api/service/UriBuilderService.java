7f4f69f1c2d13225a85d879fe739e15d04865159
PARK GI-PYO <gipyopark@gmail.com>
2023-02-03 14:18:12 +0900

    [feat] #5 : 카카오 주소 검색 API - URI builder
    검색 대상 주소가 한글이므로, API 요청을 위해 encoding 한 요청 URL 생성 객체 작성

---
+++ src/main/java/com/example/projectpoi/api/service/UriBuilderService.java
@@ -0,0 +1,24 @@
+package com.example.projectpoi.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class UriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI buildUriByAddress(String address) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        builder.queryParam("query", address);
+
+        URI uri = builder.build().encode().toUri();
+        log.info("[UriBuilderService buildUriByAddress] address: {}, uri: {}", address, uri);
+
+        return uri;
+    }
+}
