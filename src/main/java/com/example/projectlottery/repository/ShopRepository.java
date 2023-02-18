f4fc30e06fc24271067b85b2131025cb8efa49e6
PARK GI-PYO <gipyopark@gmail.com>
2023-02-18 22:30:02 +0900

    [feat] #19 : 로또 6/45 관련 repository 개발
    로또 6/45 스크랩핑 정보를 db 화하고, 서비스를 개발하기 위해 repository class 를 개발했다.
    
    `ShopRepository` 의 `findByL645YNAndScrapedDtBefore()` 메소드는 더 이상 복권을 판매하지 않는 판매점을 처리할 때 사용할 예정이다. (`scrapedDt` < 현재일 and `l645YN` = true -> 과거에는 스크랩됐지만, 현재는 없으므로 폐업 처리)

--- src/main/java/com/example/projectlottery/repository/ShopRepository.java
+++ src/main/java/com/example/projectlottery/repository/ShopRepository.java
@@ -3,5 +3,10 @@ package com.example.projectlottery.repository;
 import com.example.projectlottery.domain.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    Set<Shop> findByL645YNAndScrapedDtBefore(boolean l645YN, LocalDate scrapedDt);
 }
