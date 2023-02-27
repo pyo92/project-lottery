006ec88001519c6a4a8fb8dbcdc472bd4a411c15
PARK GI-PYO <gipyopark@gmail.com>
2023-02-27 15:59:18 +0900

    [feat] #32 : controller class 개발
    실제 브라우저에서 호출하기 위한 `controller` 클래스를 개발했다.
    
    query parameter 로 회차인 `drawNo` 를 입력받는데, 입력하지 않을 시 최신 회차의 당첨결과 페이지로 redirect 처리했다.

---
+++ src/main/java/com/example/projectlottery/controller/LottoController.java
@@ -0,0 +1,42 @@
+package com.example.projectlottery.controller;
+
+import com.example.projectlottery.dto.response.LottoResponse;
+import com.example.projectlottery.service.LottoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+@RequiredArgsConstructor
+@RequestMapping("/L645")
+@Controller
+public class LottoController {
+
+    private final LottoService lottoService;
+
+    @GetMapping
+    public String lotto(@RequestParam(required = false) Long drawNo, ModelMap map) {
+        Long latestDrawNo = lottoService.getLatestDrawNo();
+        if (drawNo == null) {
+            return "redirect:/L645?drawNo=" + latestDrawNo;
+        }
+
+        Set<Long> drawNos = new TreeSet<>(Comparator.reverseOrder());
+        for (long i = 1; i <= latestDrawNo; i++) {
+            drawNos.add(i);
+        }
+
+        LottoResponse lottoResponse = lottoService.getLottoResponse(drawNo);
+
+        map.addAttribute("drawNos", drawNos);
+        map.addAttribute("lottoResponse", lottoResponse);
+
+        return "lotto/index";
+    }
+}
