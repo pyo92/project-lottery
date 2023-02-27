be28fdd7343d17db372712d8864c9dcc096d965c
PARK GI-PYO <gipyopark@gmail.com>
2023-02-27 15:57:29 +0900

    [feat] #32 : repository, service, response dto class 개발
    로또 당첨결과 view 구현에 필요한 respository, service 클래스를 구현했다.
    
    또, 필요한 정보만 담아 view 에 전달하기 위한 `response dto` 클래스도 구현했다.

---
+++ src/main/java/com/example/projectlottery/dto/response/LottoResponse.java
@@ -0,0 +1,69 @@
+package com.example.projectlottery.dto.response;
+
+import com.example.projectlottery.domain.Lotto;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public record LottoResponse(
+        Long drawNo,
+        String drawDt,
+        int number1,
+        int number2,
+        int number3,
+        int number4,
+        int number5,
+        int number6,
+        int bonus,
+        Set<LottoPrizeResponse> lottoPrizes,
+        Set<LottoWinShopResponse> lotto1stWinShops,
+        Set<LottoWinShopResponse> lotto2ndWinShops
+) {
+
+    public static LottoResponse of(Long drawNo, String drawDt, int number1, int number2, int number3, int number4, int number5, int number6, int bonus, Set<LottoPrizeResponse> lottoPrizes, Set<LottoWinShopResponse> lottoWinShops1st, Set<LottoWinShopResponse> lottoWinShops2nd) {
+        return new LottoResponse(
+                drawNo,
+                drawDt,
+                number1,
+                number2,
+                number3,
+                number4,
+                number5,
+                number6,
+                bonus,
+                lottoPrizes,
+                lottoWinShops1st,
+                lottoWinShops2nd
+        );
+    }
+
+    public static LottoResponse from(Lotto entity) {
+        return LottoResponse.of(
+                entity.getDrawNo(),
+                entity.getDrawDt().toString(),
+                entity.getLottoWinNumber().getNumber1(),
+                entity.getLottoWinNumber().getNumber2(),
+                entity.getLottoWinNumber().getNumber3(),
+                entity.getLottoWinNumber().getNumber4(),
+                entity.getLottoWinNumber().getNumber5(),
+                entity.getLottoWinNumber().getNumber6(),
+                entity.getLottoWinNumber().getNumberB(),
+                entity.getLottoPrizes().stream()
+                        .map(LottoPrizeResponse::from)
+                        .collect(Collectors.toCollection(() ->
+                                new TreeSet<>(Comparator.comparing(LottoPrizeResponse::rank)))), //등위로 정렬
+                entity.getLottoWinShops().stream()
+                        .filter(lottoWinShop -> lottoWinShop.getRank() == 1)
+                        .map(LottoWinShopResponse::from)
+                        .collect(Collectors.toCollection(() ->
+                                new TreeSet<>(Comparator.comparing(LottoWinShopResponse::no)))), //순번으로 정렬 (1등)
+                entity.getLottoWinShops().stream()
+                        .filter(lottoWinShop -> lottoWinShop.getRank() == 2)
+                        .map(LottoWinShopResponse::from)
+                        .collect(Collectors.toCollection(() ->
+                                new TreeSet<>(Comparator.comparing(LottoWinShopResponse::no)))) //순번으로 정렬 (2등)
+        );
+    }
+}
--- src/main/java/com/example/projectlottery/repository/LottoRepository.java
+++ src/main/java/com/example/projectlottery/repository/LottoRepository.java
@@ -2,6 +2,12 @@ package com.example.projectlottery.repository;
 
 import com.example.projectlottery.domain.Lotto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LottoRepository extends JpaRepository<Lotto, Long> {
+
+    @Query("select max(l.drawNo) from Lotto l")
+    Long getLatestDrawNo(); //회차 선택 combo-box 생성 용도로 사용할 JPQL
 }
