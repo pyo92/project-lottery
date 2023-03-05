fb0e5ddf482ecdaf08b489545e9458de65213011
PARK GI-PYO <gipyopark@gmail.com>
2023-03-05 17:24:36 +0900

    [feat] #33 : 판매점 상세 정보 제공 service, controller, response dto 개발
    판매점 상세 정보를 표시하기 위한 service, controller 클래스와 view 에 전달하기 위한 response dto 를 개발했다.
    상호명, 전화번호, 취급복권, 당첨집계, 지도 및 1등/2등 배출 회차 등의 정보를 표시했다.
    1등/2등 배출 이력 목록을 클릭하면, 해당 회차 당첨결과 정보로 이동하도록 링크 처리했다.

---
+++ src/main/java/com/example/projectlottery/dto/response/shop/ShopResponse.java
@@ -0,0 +1,79 @@
+package com.example.projectlottery.dto.response.shop;
+
+import com.example.projectlottery.domain.Shop;
+import com.example.projectlottery.domain.type.LottoPurchaseType;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public record ShopResponse(
+        String name,
+        String address,
+        String tel,
+        double longitude,
+        double latitude,
+        boolean l645YN,
+        boolean l720YN,
+        boolean spYN,
+        long count1stWin,
+        long count1stWinAuto,
+        long count1stWinManual,
+        long count1stWinMix,
+        long count2ndWin,
+        Set<ShopLottoWinResponse> lotto1stWin,
+        Set<ShopLottoWinResponse> lotto2ndWin
+) {
+
+    public static ShopResponse of(String name, String address, String tel, double longitude, double latitude, boolean l645YN, boolean l720YN, boolean spYN, long count1stWin, long count1stWinAuto, long count1stWinManual, long count1stWinMix, long count2ndWin, Set<ShopLottoWinResponse> lotto1stWin, Set<ShopLottoWinResponse> lotto2ndWin) {
+        return new ShopResponse(name, address, tel, longitude, latitude, l645YN, l720YN, spYN, count1stWin, count1stWinAuto, count1stWinManual, count1stWinMix, count2ndWin, lotto1stWin, lotto2ndWin);
+    }
+
+    public static ShopResponse from(Shop entity) {
+        long count1stWinAuto = entity.getLottoWinShops().stream()
+                .filter(lottoWinShop -> lottoWinShop.getRank() == 1 && lottoWinShop.getLottoPurchaseType() == LottoPurchaseType.AUTO)
+                .count();
+        long count1stWinManual = entity.getLottoWinShops().stream()
+                .filter(lottoWinShop -> lottoWinShop.getRank() == 1 && lottoWinShop.getLottoPurchaseType() == LottoPurchaseType.MANUAL)
+                .count();
+        long count1stWinMix = entity.getLottoWinShops().stream()
+                .filter(lottoWinShop -> lottoWinShop.getRank() == 1 && lottoWinShop.getLottoPurchaseType() == LottoPurchaseType.MIX)
+                .count();
+        long count1stWin = count1stWinAuto + count1stWinManual + count1stWinMix;
+
+        long count2ndWin = entity.getLottoWinShops().stream()
+                .filter(lottoWinShop -> lottoWinShop.getRank() == 2)
+                .count();
+
+        return ShopResponse.of(
+                entity.getName(),
+                entity.getAddress(),
+                entity.getTel(),
+                entity.getLongitude(),
+                entity.getLatitude(),
+                entity.isL645YN(),
+                entity.isL720YN(),
+                entity.isSpYN(),
+                count1stWin,
+                count1stWinAuto,
+                count1stWinManual,
+                count1stWinMix,
+                count2ndWin,
+                entity.getLottoWinShops().stream()
+                        .filter(lottoWinShop -> lottoWinShop.getRank() == 1)
+                        .map(ShopLottoWinResponse::from)
+                        .collect(Collectors.toCollection(() ->
+                                new TreeSet<>(Comparator.comparing(ShopLottoWinResponse::drawNo)
+                                        .reversed()
+                                        .thenComparing(ShopLottoWinResponse::no)))),
+                entity.getLottoWinShops().stream()
+                        .filter(lottoWinShop -> lottoWinShop.getRank() == 2)
+                        .map(ShopLottoWinResponse::from)
+                        .collect(Collectors.toCollection(() ->
+                                new TreeSet<>(Comparator.comparing(ShopLottoWinResponse::drawNo)
+                                        .reversed()
+                                        .thenComparing(ShopLottoWinResponse::no))))
+        );
+    }
+}
