211068a38a90a1ba4dd69ab800e8d126188a7ac5
PARK GI-PYO <gipyopark@gmail.com>
2023-03-14 01:14:41 +0900

    [feat] #34 : shop controller 추가 기능 구현
    view 에 지역별 판매점 목록을 표시할 수 있도록 시도, 시군구 검색 결과 판매점 목록과 시도, 시군구 리스트 등을 model 에 담아 view 에 전달하도록 controller 클래스에 기능을 추가했다.

--- src/main/java/com/example/projectlottery/controller/ShopController.java
+++ src/main/java/com/example/projectlottery/controller/ShopController.java
@@ -1,13 +1,21 @@
 package com.example.projectlottery.controller;
 
 import com.example.projectlottery.dto.response.shop.ShopResponse;
+import com.example.projectlottery.service.PaginationService;
+import com.example.projectlottery.service.RegionService;
 import com.example.projectlottery.service.ShopService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -16,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ShopController {
 
     private final ShopService shopService;
+    private final RegionService regionService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String shop(@RequestParam String shopId, ModelMap map) {
@@ -23,5 +33,26 @@ public class ShopController {
         map.addAttribute("shopResponse", shopResponse);
 
         return "/shop/detail";
+    }
+
+    @GetMapping("/list")
+    public String shops(
+            @RequestParam(required = false) String state1,
+            @RequestParam(required = false) String state2,
+            @PageableDefault(sort = "address", direction = Sort.Direction.ASC) Pageable pageable,
+            ModelMap map
+    ) {
+        Page<ShopResponse> shops = shopService.getShopListResponse(state1, state2, pageable);
+        List<Integer> pagination = paginationService.getPagination(pageable.getPageNumber(), shops.getTotalPages());
+
+        List<String> state1List = regionService.getAllState1();
+        List<String> state2List = regionService.getAllState2(state1);
+
+        map.addAttribute("shops", shops);
+        map.addAttribute("pagination", pagination);
+        map.addAttribute("state1List", state1List);
+        map.addAttribute("state2List", state2List);
+
+        return "/shop/list";
     }
 }
