d31117d0ecd96e4e76c208fe5b272a04eaee21b3
PARK GI-PYO <gipyopark@gmail.com>
2023-04-06 01:50:17 +0900

    [feat] #85 : QueryDSL - repository 개발
    기본적으로 판매점마다 1등, 2등 당첨 횟수를 집계할 필요가 있는데, 이러한 부분을 포함해 querydsl 적용 범위를 확장한다.
    
    querydsl 적용을 위한 interface 와 구현체를 구현하고, 이를 상속받아 repository 를 사용할 수 있도록 했다.

--- src/main/java/com/example/projectlottery/repository/LottoRepository.java
+++ src/main/java/com/example/projectlottery/repository/LottoRepository.java
@@ -1,10 +1,11 @@
 package com.example.projectlottery.repository;
 
 import com.example.projectlottery.domain.Lotto;
+import com.example.projectlottery.repository.querydsl.LottoRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface LottoRepository extends JpaRepository<Lotto, Long> {
+public interface LottoRepository extends JpaRepository<Lotto, Long>, LottoRepositoryCustom {
 
     @Query("select max(l.drawNo) from Lotto l")
     Long getLatestDrawNo(); //회차 선택 combo-box 생성 용도로 사용할 JPQL
