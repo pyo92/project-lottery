ce29a9f706ed1345801cc1bc37646043d42481f6
PARK GI-PYO <gipyopark@gmail.com>
2023-06-06 00:27:03 +0900

    [fix] #143 : user controller
    user controller class 단위에 불필요한 `RequestMapping` annotation 이 존재해서 이를 수정한다.

--- src/main/java/com/example/projectlottery/controller/UserController.java
+++ src/main/java/com/example/projectlottery/controller/UserController.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@RequestMapping("/")
 @Controller
 public class UserController {
 
