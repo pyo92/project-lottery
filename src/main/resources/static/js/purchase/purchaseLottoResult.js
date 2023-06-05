64b49bc13f0313bcf04b3377ace20858bde16170
PARK GI-PYO <gipyopark@gmail.com>
2023-06-05 21:02:14 +0900

    [feat] #123 : 구매 내역 관련 client-side 개발
    저장된 구매 내역을 조회할 수 있는 client-side view 를 개발했다.

--- src/main/resources/static/js/purchase/purchaseLottoResult.js
+++ src/main/resources/static/js/purchase/purchaseLottoResult.js
@@ -2,21 +2,25 @@
  * load event handler
  */
 window.addEventListener('load', function () {
-    //alert message 표시
-    const serverAlert = document.querySelector('.alert.alert-success');
-    if (serverAlert != null) {
-        alert(serverAlert.textContent.trim());
-        serverAlert.remove();
-    }
+   //구매 직후 페이지 넘어온 경우 alert message 표시
+   const purchaseAlert = document.querySelector('.alert.alert-success');
+   if (purchaseAlert != null) {
+      alert(purchaseAlert.textContent.trim());
+      purchaseAlert.remove();
+   }
+
+   //구매 결과가 하나도 존재하지 않는 경우 alert message 표시
+   const purchasedNoneAlert = document.querySelector('.alert.alert-danger');
+   if (purchasedNoneAlert != null) {
+      alert(purchasedNoneAlert.textContent.trim());
+      purchasedNoneAlert.remove();
+   }
 });
 
-//////////////////////////////
-
-const confirmButton = document.getElementById('confirm-btn');
-
 /**
- * 확인 버튼 click event handler
+ * 회차 selector 변경 시, handler
  */
-confirmButton.addEventListener('click', function () {
-   window.location.href = '/'; //일회용 안내 페이지 이므로, 루트 페이지로 이동
+$('#purchase-draw-no').on('change', function () {
+   //회차 선택 시, 해당 회차로 이동
+   $('#purchase-result-search-form').submit();
 });
