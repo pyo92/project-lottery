d4335af7cea950a98acb533fe8fd686e1a5a6ab6
PARK GI-PYO <gipyopark@gmail.com>
2024-03-15 17:30:45 +0900

    [feat] #174 - user response dto 수정
    `user`, `user_role` 도메인 분리로, 하나의 사용자가 여러 role 을 소유할 수 있게 되었다.
    따라서, 단일 role 을 사용하던 방식에서 여러 role 을 사용할 수 있도록 dto 역시 수정했다.

--- src/main/java/com/example/projectlottery/dto/response/UserResponse.java
+++ src/main/java/com/example/projectlottery/dto/response/UserResponse.java
@@ -1,21 +1,49 @@
 package com.example.projectlottery.dto.response;
 
 import com.example.projectlottery.domain.User;
+import com.example.projectlottery.domain.UserRole;
 import com.example.projectlottery.domain.type.UserRoleType;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
 
 public record UserResponse(
         String userId,
-        UserRoleType userRoleType
+        List<UserRoleType> userRoleTypes,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
 ) {
 
-    public static UserResponse of(String userId, UserRoleType userRoleType) {
-        return new UserResponse(userId, userRoleType);
+    public static UserResponse of(String userId, List<UserRoleType> userRoleTypes, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        return new UserResponse(userId, userRoleTypes, createdAt, modifiedAt);
     }
 
     public static UserResponse from(User entity) {
         return UserResponse.of(
                 entity.getUserId(),
-                entity.getUserRoleType()
+                entity.getUserRoles()
+                        .stream()
+                        .sorted(Comparator.comparing(UserRole::getOrd))
+                        .map(r -> UserRoleType.valueOf(r.getName()))
+                        .toList(),
+                entity.getCreatedAt(),
+                entity.getModifiedAt()
         );
+    }
+
+    public String toStringUserRole() {
+        return userRoleTypes.contains(UserRoleType.ROLE_ADMIN) ? "관리자" : "사용자";
+    }
+
+    public String toStringCreatedAt() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return createdAt.format(formatter);
+    }
+
+    public String toStringModifiedAt() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return modifiedAt.format(formatter);
     }
 }
