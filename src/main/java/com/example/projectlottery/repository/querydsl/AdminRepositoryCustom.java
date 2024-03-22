357682dd56e5c4c8c32602fb85fa3aaabcb46b38
PARK GI-PYO <gipyopark@gmail.com>
2024-03-22 20:00:25 +0900

    [feat] #175 - admin repository (selenium info)
    selenium chrome driver 목록을 조회하도록 repository 계층에 메서드를 생성했다.
    다만, 해당 목록은 db 에서 관리되는 항목이 아니며, docker container 목록을 편의상 select 시켰다.

--- src/main/java/com/example/projectlottery/repository/querydsl/AdminRepositoryCustom.java
+++ src/main/java/com/example/projectlottery/repository/querydsl/AdminRepositoryCustom.java
@@ -6,5 +6,15 @@ import java.util.List;
 
 public interface AdminRepositoryCustom {
 
+    /**
+     * Scrap api 목록과 영향도 있는 테이블의 최종 업데이트 일시 조회 - 관리자용
+     * @return Scrap api 목록 (+ 최종 업데이트 일시)
+     */
     List<APIResponse> getAllAPIModifiedAt();
+
+    /**
+     * Selenium chrome driver 목록 조회 - 관리자용
+     * @return Selenium chrome driver 목록
+     */
+    List<String> getAllSeleniumChromeDriver();
 }
