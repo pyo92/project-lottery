e5aa1d136862a043b2df1abd91985ee44242fb2c
PARK GI-PYO <gipyopark@gmail.com>
2025-04-27 17:25:55 +0900

    [feat] ssl 적용
    ssl 적용을 위한 포트 수정 및 redirect config 파일 추가

---
+++ src/main/java/com/example/projectlottery/config/HttpRedirectConfig.java
@@ -0,0 +1,27 @@
+package com.example.projectlottery.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpRedirectConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
